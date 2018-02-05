2fec3e7c827c452707e73d5a00c20cad5db1ef33
suisuiyang <30796619+suisuiyang@users.noreply.github.com>
2018-02-05 11:35:49 +0800

    s

---
+++ hmpro/zhangxiyang/homework/greatestcommondivisor.py
@@ -0,0 +1,17 @@
+a = input("输入第一个数字")
+b = input("输入第二个数字")
+a = int(a)
+b = int(b)
+def greatestcommondivisor(a, b):
+   if a > b:
+       smaller = b
+   else:
+       smaller = a
+
+   for i in range(1,smaller):
+       if((a % i == 0) and (b % i == 0)):
+           greatestcommondivisor = i
+
+   return greatestcommondivisor
+
+print(greatestcommondivisor(a,b))
