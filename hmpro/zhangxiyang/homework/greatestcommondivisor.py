d5d7245909767d7e1bcd65e9bbea3a1ab50589c4
suisuiyang <30796619+suisuiyang@users.noreply.github.com>
2018-02-05 12:07:38 +0800

    s

--- hmpro/zhangxiyang/homework/greatestcommondivisor.py
+++ hmpro/zhangxiyang/homework/greatestcommondivisor.py
@@ -10,7 +10,7 @@ def greatestcommondivisor(a, b):
    else:
        smaller = a
 
-   for i in range(1,smaller):
+   for i in range(1,smaller+1):
        if((a % i == 0) and (b % i == 0)):
            greatestcommondivisor = i
 
